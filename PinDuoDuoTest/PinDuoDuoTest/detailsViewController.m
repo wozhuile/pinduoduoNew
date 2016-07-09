8a406f559b68e9badda0087ff6a6681c3ded1048
MirChangBin <mac@macdeMacintosh.local>
2016-07-09 14:51:15 +0800

    contentMode的使用
    #pragma mark
    上边用数组遍历也会出来问题的，断点发现不是就一次。，这个就咬考虑效率问题了。。因为不要遍历，我直接用这个数组_showArray
    objectAtIndex:indexPath.row就不会错，，用遍历也不会错，，，但是用上边那个直接obj就会错，，是不是用id的原因？？试
    试改对应对象看看，不行，，不知道在cell类里边创建会怎么样，，，以后遇到这类型的，数组里边放URL的，，要给图片赋值的，就要[_showArr
    ay objectAtIndex:indexPath.row]方法，尽量不需要遍历了如果不需要取值。
    
                //这种是可以的了。。但是理解不够，，还是没达到效果
                //img.contentMode=UIViewContentModeScaleAspectFit;
                //设置个颜色来看看 效果
                //NSLog(@"11====%f",img.frame.size.width);
                NSLog(@"11====%f",img.frame.size.height);
    
                //NSLog(@"11====%f",img.frame.size.width);
    
                img.backgroundColor=[UIColor redColor];
                //img.contentMode=UIViewContentModeScaleAspectFit;
    #warning 用这个模型是保持了比例，，但是还需要传进来高度和宽度来具体计算的，，它们都不是唯一一样的，，要不就让服务器给一样宽度和高度的。
                img.contentMode=UIViewContentModeScaleAspectFill;
               // NSLog(@"22====%f",img.frame.size.width);
    
                 NSLog(@"22====%f",img.frame.size.height);
    
                [img sd_setImageWithURL:[NSURL URLWithString:[_showArray
    objectAtIndex:indexPath.row]] placeholderImage:[UIImage
    imageNamed:@"default_mall_logo"]];
                NSLog(@"333====%f",img.frame.size.height);
                //用.clipsToBounds = YES;可以使图片占满整个父容器，并且不变形。
    
                //img.clipsToBounds=YES;
    
       }];

--- PinDuoDuoTest/PinDuoDuoTest/detailsViewController.m
+++ PinDuoDuoTest/PinDuoDuoTest/detailsViewController.m
@@ -138,35 +138,166 @@
      1,最上边和最下边图片，都在gallery里边，type是1的时候，就是滚动的，是2的时候，就是下边展示的
      2,
      */
+    //临时滚动图片
+    NSMutableArray*array=[[NSMutableArray alloc]init];
+    //临时展示图片
+    NSMutableArray*showArray=[[NSMutableArray alloc]init];
 #warning 不会做就分布做，现在就先处理这个图片，这个gallery数组,得到就去布局先，得到什么先做什么就好了
     //遍历方法
     [homeDetail.gallery enumerateObjectsUsingBlock:^(HomeGallery*  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         //根据索引idx和数组类型来做
+        
+        
+        
         if (obj.type==1) {
-            [_scrollArray addObject:obj.url];
-             //NSLog(@"_scrollArray====%@",_scrollArray);
+         //[_scrollArray addObject:obj.url];
+         [array addObject:obj.url];
+           
+                //int  count=(int)array.count;
+            
+#pragma mark 在里边这样反排序，就会报错崩溃，崩溃是因为我大于等于0，，然后这里出来的排序还是之前样子，，还有一点就是，这个遍历进行多少次，就重复多少次数组添加，之前是4张图片，出来后就12张等等了。。。。在外边一样的代码，效果出来了，还没有bug，，这就是遍历的核心重点，不是随便逗可以用
+            
+//        for (int i=count; i>0; i--) {
+//           NSMutableArray*selectArray= [array objectAtIndex:i-1];
+//            [_scrollArray addObject:selectArray];
+//            
+//           }
+            //[_scrollArray insertObject:obj.url atIndex:idx];
+            ///
+            
+            //[array enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+               
+               // [_scrollArray addObject:obj];
+           // }];
+            
+            
+            
             
             
         }
         if (obj.type==2) {
 #pragma mark 输出，断点试试看数组都对没有
-            [_showArray addObject:obj.url];
+            [showArray addObject:obj.url];
             //NSLog(@"_showArray====%@",_showArray);
         }
         
     }];
     
     
-#pragma mark 第二步，第一区固定死值，用字典来做？
     
+    
+#pragma mark 完美排序了，前后元素调换过来
+   int  count=(int)array.count;
+   
+   for (int i=count; i>0; i--) {
+       NSMutableArray*selectArray= [array objectAtIndex:i-1];
+       [_scrollArray addObject:selectArray];
+       
+   }
+//这里这样处理后，下边show都没有图片了。。全部变成滚动了。。。那是因为 [_scrollArray addObject:selectArray];里边美誉修改。虽然看起来上边这样也不错，因为点击进来的是气质美女。。。但是效果，，，，就是bug
+    int  ShowCount=(int)showArray.count;
+    
+    for (int i=ShowCount; i>0; i--) {
+        NSMutableArray*selectArray= [showArray objectAtIndex:i-1];
+        //[_scrollArray addObject:selectArray];
+        [_showArray addObject:selectArray];
+        
+    }
+
+    
+    
+    
+#pragma mark 第二步，第一区固定死值，用字典来做？
     
     
     
     [_detailTableView reloadData];
     
     
+   
+    
+    
     
 }
+
+/*void bubbleSort(int arrry[]);
+
+int main(int argc, const char * argv[]) {
+    
+    int numberArray[20] = {9,2,10,399,339,33,335,567,353,246,567,896,678,1234,567,78,682,13,4 };
+    
+    bubbleSort(numberArray);
+    
+    for (int i = 0; i < 20; i++) {
+        printf("%d位数字是%d\n",i,numberArray[i]);
+    }
+    
+    return 0;
+}
+*/
+#warning 冒泡排序，滚动图片和展示图片排序
+void bubbleSort(int arrry[] ){
+    
+    for (int i = 0; i < 20; i++)
+    {
+        
+        for (int j = i; j < 20; j++)
+        {
+            if (arrry[i] > arrry[j] )
+            {
+                // 大数下沉策略
+                int temp = arrry[i];
+                arrry[i] = arrry[j];
+                arrry[j] = temp;
+            }
+        }
+    }
+    
+}
+void bubble_sory(int array[], int count) {
+    
+    for (int i = 0; i < count - 1; i++) {
+        
+        int didSwap = 0;
+        for (int j = 0; j < count-1-i; j++) {
+            
+            if (array[j] > array[j+1]) {
+                
+                // 交换数值
+                array[j] = array[j]+array[j+1];
+                array[j+1] = array[j]-array[j+1];
+                array[j] = array[j]-array[j+1];
+                
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0) {
+            return;
+        }
+    }
+}
+
+
+//-(void)bubblesort:(NSArray*)array
+//{
+//    for (int i = 0; i < array.count; i++)
+//    {
+//        
+//        for (int j = i; j < array.count; j++)
+//        {
+//            if (array[i]> array[j])
+//            {
+//                // 大数下沉策略
+//                int temp = array[i];
+//                array[i] = array[j];
+//                array[j] = temp;
+//            }
+//        }
+//    }
+//
+//}
+
+
 -(void)failToSendDatailData:(detailModel *)detailModel error:(NSError *)error
 {
     NSLog(@"详情error%@",error);
@@ -291,7 +422,7 @@
 
         UIImageView*oldImage=[cell.contentView viewWithTag:100];
         [oldImage removeFromSuperview];
-        UIImageView*img=[[UIImageView alloc]initWithFrame:CGRectMake(15, 5, CGRectGetWidth(self.view.frame)-30,  350)];
+        UIImageView*img=[[UIImageView alloc]initWithFrame:CGRectMake(0, 5, CGRectGetWidth(self.view.frame),  530)];//之前是350.。
         
         img.tag=100;
         
@@ -307,12 +438,37 @@
             //NSLog(@"------%@",obj);
             //[img sd_setImageWithURL:[NSURL URLWithString:obj] placeholderImage:[UIImage imageNamed:@"default_mall_logo"]];
 #pragma mark 上边用数组遍历也会出来问题的，断点发现不是就一次。，这个就咬考虑效率问题了。。因为不要遍历，我直接用这个数组_showArray objectAtIndex:indexPath.row就不会错，，用遍历也不会错，，，但是用上边那个直接obj就会错，，是不是用id的原因？？试试改对应对象看看，不行，，不知道在cell类里边创建会怎么样，，，以后遇到这类型的，数组里边放URL的，，要给图片赋值的，就要[_showArray objectAtIndex:indexPath.row]方法，尽量不需要遍历了如果不需要取值。
-            [img sd_setImageWithURL:[NSURL URLWithString:[_showArray objectAtIndex:indexPath.row]] placeholderImage:[UIImage imageNamed:@"default_mall_logo"]];
             
+            //这种是可以的了。。但是理解不够，，还是没达到效果
+            //img.contentMode=UIViewContentModeScaleAspectFit;
+            //设置个颜色来看看 效果
+            //NSLog(@"11====%f",img.frame.size.width);
+            NSLog(@"11====%f",img.frame.size.height);
+   
+            //NSLog(@"11====%f",img.frame.size.width);
+
+            img.backgroundColor=[UIColor redColor];
+            //img.contentMode=UIViewContentModeScaleAspectFit;
+#warning 用这个模型是保持了比例，，但是还需要传进来高度和宽度来具体计算的，，它们都不是唯一一样的，，要不就让服务器给一样宽度和高度的。
+            img.contentMode=UIViewContentModeScaleAspectFill;
+           // NSLog(@"22====%f",img.frame.size.width);
+
+             NSLog(@"22====%f",img.frame.size.height);
+            
+            [img sd_setImageWithURL:[NSURL URLWithString:[_showArray objectAtIndex:indexPath.row]] placeholderImage:[UIImage imageNamed:@"default_mall_logo"]];
+            NSLog(@"333====%f",img.frame.size.height);
+            //用.clipsToBounds = YES;可以使图片占满整个父容器，并且不变形。
+            
+
+            //img.clipsToBounds=YES;
+            
+
    }];
      
         
         
+        //看看图片模型的效果
+        cell.backgroundColor=[UIColor orangeColor];
         
         
         return cell;
@@ -375,7 +531,7 @@
     }
     //展示的图片，一般10张，每张大概先350到400.。参数给有，，这里就任性先
     if (indexPath.section==4) {
-        return 350;
+        return 530;//之前是350，，不够高度，，我还没有传图片的高度和宽度进来计算。。要自动那个模型来设计，会保持原来图片的宽高比例的。。
     }
     //推荐..看数组给的..先大概给个全屏幕高
     else
